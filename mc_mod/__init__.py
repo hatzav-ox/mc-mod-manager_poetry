88eb2de7fb6cc149ed5600cee5b9bcbb9332ec48
Brenek Harrison <brenekharrison@gmail.com>
2021-05-03 22:24:37 -0600

    Update a profile

--- mc_mod/__init__.py
+++ mc_mod/__init__.py
@@ -1,12 +1,26 @@
-from mc_mod import curse_forge
+# Other library imports
 import os
+from typing import Dict
 from json import load
 from pathlib import Path
+from shutil import move as shutil_move
 from sys import argv
+
+# Own library imports
+from . import curse_forge
 
 __version__ = "0.0.1-alpha.1"
 
+# TODO: Cross-platform locations
 dot_minecraft = Path(os.getenv("APPDATA")) / ".minecraft"
+config_dir = Path(os.getenv("APPDATA")) / "mc_mod"
+profile_jars_dir = Path(os.getenv("LOCALAPPDATA")) / "mc_mod/profiles"
+
+# Ensure our directories are valid
+config_dir.mkdir(parents=True, exist_ok=True)
+profile_jars_dir.mkdir(parents=True, exist_ok=True)
+
+mod_providers: Dict = {"curse_forge": curse_forge}
 
 def cli():
 	"""cli parses out sys.argv and dispatches the appropriate commands.
@@ -16,13 +30,26 @@ def cli():
 	if command == "switch":
 		wanted_profile_name = argv[2] # mc_mod switch [profile_name]
 
-		# TODO: Load profile from APPDATA/mc_mod/profiles/{wanted_profile_name}.json
-		with Path(f"{os.getenv('APPDATA')}/mc_mod/profiles/{wanted_profile_name}.json").open("r") as f:
+		# Load profile from APPDATA/mc_mod/profiles/{wanted_profile_name}.json
+		with (config_dir / f"profiles/{wanted_profile_name}.json").open("r") as f:
 			profile_obj = load(f)
 
-		# TODO: Update mod jars in cache
-		curse_forge.download_mod(profile_obj["mods"][0]["info"]["id"])
+		# TODO: Remove mod jars from dot_minecraft/mods
 
-		# TODO: Remove mod jars from dot_minecraft
+		# TODO: Copy profile jars from LOCALAPPDATA to dot_minecraft/mods
 
-		# TODO: Copy profile jars to dot_minecraft
+	elif command == "update":
+		wanted_profile_name = argv[2] # mc_mod update [profile_name]
+
+		# Load profile from APPDATA/mc_mod/profiles/{wanted_profile_name}.json
+		with (config_dir / f"profiles/{wanted_profile_name}.json").open("r") as f:
+			profile_obj = load(f)
+
+		# Download up-to-date jars
+		for mod in profile_obj["mods"]:
+			file_location: Path = mod_providers[mod["type"]].download_mod(mod["info"])
+			print(file_location)
+
+			# Move jars to profile_jars_dir/profiles/{wanted_profile_name}/{mod_file_name}
+			(profile_jars_dir / wanted_profile_name).mkdir(parents=True, exist_ok=True)
+			shutil_move(str(file_location), str(profile_jars_dir / wanted_profile_name / file_location.name))
--- mc_mod/curse_forge/__init__.py
+++ mc_mod/curse_forge/__init__.py
@@ -1,34 +1,67 @@
 """curse_forge provides a basic API for downloading mods from Curse Forge.
 """
 
+import os
 import requests
+from typing import Tuple
+from pathlib import Path
 
 latest_mc_version = "1.16.5"
 
-def download_mod(id: int) -> str:
+def download_mod(info) -> Path:
+	id = info["id"]
+	name = info["name"]
+
 	r = requests.get(f"https://addons-ecs.forgesvc.net/api/v2/addon/{id}", headers={"User-Agent": "Mozilla/5.0"})
 
 	r.raise_for_status() # I'm not in love with throwing an error, but I guess this is more robust than checking status_code ourselves.
 
 	json_obj = r.json()
 
-	latest_file_id = 0
-	for obj in json_obj["gameVersionLatestFiles"]:
+	if info["check_file_name"] == None:
+		file_id, filename = _extract_file(json_obj["gameVersionLatestFiles"])
+	else:
+		file_id, filename = _extract_file_with_file_name_check(json_obj["gameVersionLatestFiles"], info["check_file_name"])
+
+	file_download_url = _gen_download_url(file_id, filename)
+
+	r = requests.get(file_download_url)
+
+	out_file = Path(f"{os.getenv('LOCALAPPDATA')}/mc_mod/cache/curse_forge/{name}.jar")
+
+	out_file.parent.mkdir(parents=True, exist_ok=True)
+
+	with out_file.open("wb") as f:
+		f.write(r.content)
+
+	return out_file
+
+def _extract_file(json_obj) -> Tuple[int, str]:
+	file_id = 0
+	filename = ""
+	for obj in json_obj:
 		if obj["gameVersion"] == latest_mc_version:
-			latest_file_id = obj["projectFileId"]
+			file_id = obj["projectFileId"]
+			filename = obj["projectFileName"]
 			break
 	else:
 		raise RuntimeError(f"No files for Minecraft {latest_mc_version} found!")
 
-	file_download_url = ""
-	for obj in json_obj["latestFiles"]:
-		if obj["id"] == latest_file_id:
-			file_download_url = obj["downloadUrl"]
+	return file_id, filename
+
+def _extract_file_with_file_name_check(json_obj, search_file_name: str) -> Tuple[int, str]:
+	file_id = 0
+	filename = ""
+	for obj in json_obj:
+		if obj["gameVersion"] == latest_mc_version and search_file_name.lower() in obj["projectFileName"].lower():
+			file_id = obj["projectFileId"]
+			filename = obj["projectFileName"]
 			break
 	else:
-		raise RuntimeError("Could not find file in latestFiles.")
+		raise RuntimeError(f"No files for Minecraft {latest_mc_version} found!")
 
-	r = requests.get(file_download_url)
+	return file_id, filename
 
-	with open("test.jar", "wb") as f:
-		f.write(r.content)
+def _gen_download_url(id: int, filename: str) -> str:
+	str_id = str(id)
+	return f"https://edge.forgecdn.net/files/{str_id[:4]}/{str_id[4:]}/{filename}"
