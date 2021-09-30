b28eba7254a94a41206c8c70e30dba8ef128c93e
Brenek Harrison <brenekharrison@gmail.com>
2021-09-30 12:23:37 -0600

    Update help message

--- mcmm/__init__.py
+++ mcmm/__init__.py
@@ -12,44 +12,46 @@ from .plugin_internal import load_providers
 
 __version__ = "0.0.5"
 
+
 def cli():
-	"""cli parses out sys.argv and dispatches the appropriate commands.
-	"""
-	colorama_init()
+    """cli parses out sys.argv and dispatches the appropriate commands.
+    """
+    colorama_init()
 
-	if "--help" in argv:
-		help()
-		return
+    if "--help" in argv:
+        help()
+        return
 
-	if "--version" in argv:
-		version()
-		return
+    if "--version" in argv:
+        version()
+        return
 
-	if len(argv) == 1:
-		print(f"[{Fore.RED}ERROR{Fore.RESET}] Expected at least one argument.")
-		return
+    if len(argv) == 1:
+        print(f"[{Fore.RED}ERROR{Fore.RESET}] Expected at least one argument.")
+        return
 
-	command = argv[1] # mcmm [command]
+    command = argv[1]  # mcmm [command]
 
-	if command == "activate":
-		_activate_dispatcher(argv[2:])
+    if command == "activate":
+        _activate_dispatcher(argv[2:])
 
-	elif command == "deactivate":
-		_deactivate_dispatcher(argv[2:])
+    elif command == "deactivate":
+        _deactivate_dispatcher(argv[2:])
 
-	elif command == "download":
-		mod_providers = load_providers(aggregate_mod_provider_list())
-		_download_dispatcher(argv[2:], mod_providers)
+    elif command == "download":
+        mod_providers = load_providers(aggregate_mod_provider_list())
+        _download_dispatcher(argv[2:], mod_providers)
 
-	elif command == "generate":
-		mod_providers = load_providers(aggregate_mod_provider_list())
-		_generate_dispatcher(argv[2:], mod_providers)
+    elif command == "generate":
+        mod_providers = load_providers(aggregate_mod_provider_list())
+        _generate_dispatcher(argv[2:], mod_providers)
 
-	elif command == "list":
-		_list_dispatcher(argv[2:])
+    elif command == "list":
+        _list_dispatcher(argv[2:])
+
 
 def help():
-	print(f"""Minecraft Mod Manager (mcmm) {__version__} Help
+    print(f"""Minecraft Mod Manager (mcmm) {__version__} Help
 
 Commands:
     - activate <profile> - Removes all existing jars from the .minecraft/mods folder and copies the jars associated with <profile> into .minecraft/mods.
@@ -63,21 +65,25 @@ Commands:
 
     - list               - Lists all available profiles.
 
+    - modify <profile>   - Modify profile settings.
+
 """)
 
+
 def version():
-	print(f"Minecraft Mod Manager (mcmm) Version {__version__}")
+    print(f"Minecraft Mod Manager (mcmm) Version {__version__}")
+
 
 def aggregate_mod_provider_list() -> List[Union[str, ModuleType]]:
-	internal_mps = [curse_forge, optifine, github, modrinth]
-	config_dir = gen_config_dir()
-	config_file = config_dir / "config.json"
+    internal_mps = [curse_forge, optifine, github, modrinth]
+    config_dir = gen_config_dir()
+    config_file = config_dir / "config.json"
 
-	if not config_file.exists():
-		with config_file.open("w") as f:
-			f.write(r'{"mod_providers": []}')
+    if not config_file.exists():
+        with config_file.open("w") as f:
+            f.write(r'{"mod_providers": []}')
 
-	with config_file.open("r") as f:
-		user_conf = load(f)
+    with config_file.open("r") as f:
+        user_conf = load(f)
 
-	return user_conf["mod_providers"] + internal_mps
+    return user_conf["mod_providers"] + internal_mps
